fe66662fa513c811a4001219a47e2717f3dd8fbb
Михаил Синёв <msinev@masterdm.ru>
2023-05-19 01:22:11 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int cBalance = 100;
+       int rAmount = 1001;
+       int bonus ;
+       int balance ;
+       if (rAmount > 1000) { bonus = (rAmount / 100);
+         balance = (cBalance + rAmount + bonus);
+       } else {
+           balance = (cBalance + rAmount);
+           bonus = 0;
+       }
+
+       System.out.println("Итоговый счёт - " + balance);
+       System.out.println("Бонус - " + bonus);
+    }
+}
